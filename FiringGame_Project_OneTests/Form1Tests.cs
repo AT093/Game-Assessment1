132888afcb0cb91b69bd67e3b1bcb4b0862db599
AT093 <Akshaythakur3020@gmail.com>
2021-05-07 10:49:19 +1200

    Added initial Code

---
+++ FiringGame_Project_OneTests/Form1Tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FiringGame_Project_One;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiringGame_Project_One.Tests
+{
+    // 2 unit tests 
+
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void Form1Test()
+        {
+            GamePlan gamePlan = new GamePlan();
+            if (gamePlan.show()==0) {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod()]
+        public void Form2Test()
+        {
+            GamePlan gamePlan = new GamePlan();
+            gamePlan.genNo();
+            if (gamePlan.show() > 0)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+
+    }
+}
